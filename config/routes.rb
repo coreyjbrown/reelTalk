9aff59a29237d578b744ccf26d69015fad812e73
coreyjbrown <coreyjbrown@gmail.com>
2015-03-08 21:37:59 -0400

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'searches/show'
 
   get 'searches/index'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309005713) do
+ActiveRecord::Schema.define(version: 20150309012748) do
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
